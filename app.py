f50b6bc528569f721418e7ab35137b4e022226f8
Guspex <62615622+Guspex@users.noreply.github.com>
2025-05-13 17:17:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,7 @@ def extrair_cartas_ligamagic(url):
     try:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
+
         chrome_options.binary_location = "/usr/bin/chromium"
     
         driver = webdriver.Chrome(options=chrome_options)
