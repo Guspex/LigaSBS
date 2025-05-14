29b9c4601289ddb36526f74de6f87c9fcac06ecb
André <andre.friedrich@bplus.com.br>
2025-05-14 13:51:57 -0300

    Atualização de status

--- app.py
+++ app.py
@@ -51,13 +51,16 @@ st.set_page_config(page_title="Troca de Cartas Magic", layout="wide")
 st.title("💬 Plataforma de Troca e Venda de Cartas - Magic: The Gathering")
 
 # Carrega dados da planilha
-with st.status("🔄 Carregando dados da planilha...", expanded=True) as status:
-    cliente = autenticar_planilha()
-    planilha = cliente.open_by_url("https://docs.google.com/spreadsheets/d/1FmicnHU9caYH0NrxO1W49OyyJsfu-vYTKd9rzkyzZ7E/edit#gid=0")
-    aba = planilha.get_worksheet(0)
-    dados = aba.get_all_records()
-    status.update(label="✅ Dados carregados com sucesso!", state="complete")
-    time.sleep(3)
+status = st.status("🔄 Carregando dados da planilha...")
+
+cliente = autenticar_planilha()
+planilha = cliente.open_by_url("URL DA PLANILHA")
+aba = planilha.get_worksheet(0)
+dados = aba.get_all_records()
+
+status.success("✅ Dados carregados com sucesso!")
+time.sleep(5)
+status.empty()
 
 # =========== CAMPO DE BUSCA POR CARTA =============
 st.header("🔎 Buscar carta por nome")
